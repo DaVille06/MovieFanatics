0a82dc3524f7b21cddc710e55a81c788c2ca40d5
Nick Roberts <roberts.nicholas88@gmail.com>
2023-07-28 21:41:33 -0400

    add rough notes for implementation

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+* create a movies obj
+* ask to movies object to add a movie by providing name, rating, and watched count
+* ask movies obj to increment the watched count by 1 for a movie with a given name
+* ask moves obj to display all movies
+* display error if movie is already in collection
+* display error if increment count for movie not in collection
+*/
+
 int main(int argc, char* argv[])
 {
 	return 0;
