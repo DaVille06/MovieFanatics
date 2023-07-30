8fff5aa8230e86a3a0f8c8144a459b7c80c71850
Nick Roberts <roberts.nicholas88@gmail.com>
2023-07-29 22:38:22 -0400

    switch to a map for collection

--- Movies.cpp
+++ Movies.cpp
@@ -1,1 +1,26 @@
 #include "Movies.h"
+
+void Movies::addMovie(std::string, MovieRating rating, int watchCount)
+{
+	// add to map
+}
+
+void Movies::watchMovie(std::string& name)
+{
+	// increment count of movie
+	// check if movie is in list
+	// if so increment
+	if (m_movieCollection.count("name") > 0)
+	{
+		m_movieCollection["name"].m_watchedCount++;
+	}
+	else
+	{
+		std::perror("The movie you entered is not in the collection. Please add it first.");
+	}
+}
+
+void Movies::displayMovies()
+{
+
+}
--- Movies.h
+++ Movies.h
@@ -1,4 +1,18 @@
 #ifndef MOVIES_H
 #define MOVIES_H
 
+#include <map>
+#include "Movie.h"
+
+class Movies
+{
+public:
+	Movies();
+	void addMovie(std::string&, MovieRating& rating, int watchCount);
+	void watchMovie(std::string& name);
+	void displayMovies();
+private:
+	std::map<std::string, Movie> m_movieCollection;
+};
+
 #endif // !MOVIES_H
